a647af307bf669bd3da11d944cfdbf1074f5ec4d
mbernhardt6 <mbernhardt6@gmail.com>
2015-02-21 23:49:11 -0800

    Fixing slight error in mailer.py
    Mailer module was not sending message as part of email. Fixing this
    omission.

--- mailer.py
+++ mailer.py
@@ -3,7 +3,7 @@ import smtplib
 def sendMail(recipient, sender, subject, message):
   host = "mail.transcendedlife.local"
   
-  body = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (sender, recipient, subject)
+  body = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (sender, recipient, subject, message)
   
   smtpObj = smtplib.SMTP(host)
   smtpObj.sendmail(sender, recipient, body)
