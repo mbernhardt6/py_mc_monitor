9c02b72ca03331fe67841c58272480a5f8033652
mbernhardt6 <mbernhardt6@gmail.com>
2015-03-09 22:29:41 -0700

    Complete Rewrite
    Rewrite script to run in background rather than on demand via cron.
    State is kept in variables rather than in files other than nocheck.

---
+++ filer.py
@@ -0,0 +1,103 @@
+# Homegrown Modules
+import logger
+
+# Python Modules
+import glob
+import os
+import sys
+
+
+def countLines(filename):
+  """Count the number of lines in a file.
+
+  Args:
+    filename: File to count lines of.
+
+  Returns:
+    Int value of the number of lines in a file.
+  """
+  try:
+    with open(filename, 'r') as file:
+      for i, l in enumerate(file):
+        pass
+    file.close()
+    return i + 1
+  except:
+    return 0
+
+
+def ReadFileData(target_file_name, log):
+  """Read data from a file ignoring commented out lines.
+
+  Args:
+    target_file_name: Filename of file to read.
+
+  Returns:
+    Contents of file as a set.
+  """
+  file_contents = set()
+  try:
+    target_file = open(target_file_name, 'r')
+    for line in target_file:
+      if line.encode('utf-8')[:1] != '#':
+        file_contents.add(line.encode('utf-8').replace('\n', ''))
+    target_file.close()
+  except:
+    logger.logMessage(log, "WARNING: Unspecified error processing %s" %
+        target_file_name)
+  return file_contents
+
+
+def tailFile(filename, tailCount):
+  """Trim a file to a set line count via tail.
+
+  Args:
+    filename: Name of file to trim.
+    tailCount: Number of lines to trim to.
+  """
+  length = countLines(filename)
+  oldFile = open(filename, 'r')
+  start = length - (int(tailCount) + 1)
+  if (start > 0):
+    arrOutFile = []
+    for i, line in enumerate(oldFile):
+      if (i > start):
+        arrOutFile.append(line)
+    oldFile.close()
+    newFile = open(filename, 'wb')
+    for line in arrOutFile:
+      if len(line) > 0:
+        newFile.write(line)
+    newFile.close()
+  else:
+    oldFile.close()
+    return 0
+
+
+def SetPid(pid_file, log):
+  """Function to handle check and setting pid files.
+
+  If pid_file doesn't exist, this function will setit and return True. If the
+  pid_file already exists it will return False.
+
+  Args:
+    pid_file: File to check for and possibly set.
+    log: script log file.
+
+  Returns:
+    Boolean if pid_file has been set or not.
+  """
+  if os.path.isfile(pid_file):
+    check_pid = ReadFileData(pid_file, log)
+    try:
+      os.getpgid(int(list(check_pid)[0]))
+      logger.logMessage(log, "%s exists, exiting." % pid_file)
+      return False
+    except:
+      try:
+        os.unlink(pid_file)
+      except:
+        pass
+  pid = str(os.getpid())
+  file(pid_file, 'w').write(pid)
+  return True
--- mc_monitor.py
+++ mc_monitor.py
@@ -1,58 +1,70 @@
 #!/usr/local/bin/python2.7
 
-#Homegrown Modules
+# Homegrown Modules
+import filer
 import logger
 import mailer
 import nettools
-#Python Modules
+
+# Python Modules
 import os
 import subprocess
+import time
 
-#Variables
+# Variables
 server = "mortimer"
 port = "25565"
 recipient = "mark@transcendedlife.local"
 sender = "feros@transcendedlife.local"
 log = "/var/log/mc_monitor.log"
-basepath = "/home/mark/mc_monitor/"
-reboot = basepath + ".reboot"
-followup = basepath + ".followup"
-nocheck = basepath + "nocheck"
-mute = basepath + "mute"
+base_path = "/home/mark/mc_monitor"
+nocheck = base_path + "nocheck"
+pid_file = "/tmp/mc_monitor.pid"
+wait_time = 300
 
 if __name__ == "__main__":
-  if not(os.path.isfile(nocheck)):
-    status = nettools.checkPort(server, port)
-    logger.logMessage(log, "Status returned %s" % status)
-    if (status != "open"):
-      if (os.path.isfile(followup) and not(os.path.isfile(mute))):
-        logger.logMessage(log, "%s restart attempted. Muting until restore." % server)
-        mailer.sendMail(recipient, sender,
-                        "Minecraft Server Restart Failed - Muting until restore",
-                        "%s restart failed.\r\nMuting until restore." % server)
-        open(mute, 'a').close()
-        os.remove(followup)
-      if (os.path.isfile(reboot)):
-        result = subprocess.Popen("ssh mc@%s \"./forcestop_start.sh\"" % server, shell=True)
-        logger.logMessage(log, "Server restart attempted.")
-        logger.logMessage(log, "%s" % result)
-        if not(os.path.isfile(mute)):
-          mailer.sendMail(recipient, sender,
-                          "Minecraft Server Down - Restart Attempted",
-                          "%s was not responding and a restart has been attempted.\r\nCheck logs for details." % server)
-        os.remove(reboot)
-        open(followup, 'a').close()
-      else:
-        open(reboot, 'a').close()
-        logger.logMessage(log, "Server may be down. No reboot.")
-    else:
-      logger.logMessage(log, "Server Check OK")
-      if (os.path.isfile(reboot)):
-        os.remove(reboot)
-      if (os.path.isfile(followup)):
-        mailer.sendMail(recipient, sender,
-                        "Server Restart Successful",
-                        "%s restart successful.\r\nServer is back online." % server)
-        os.remove(followup)
-      if (os.path.isfile(mute)):
-        os.remove(mute)
+  # Is a reboot required?
+  reboot = False
+  # Will the script follow up after reboot?
+  followup = False
+  # Is the script muted from sending email?
+  mute = False
+  if filer.SetPid(pid_file, log):
+    logger.logMessage(log, "Pid file set. Starting monitor.")
+    while 1:
+      if not (os.path.isfile(nocheck)):
+        status = nettools.checkPort(server, port)
+        if (status != "open"):
+          if followup and not mute:
+            logger.logMessage(log, "%s restart attempted. Muting until restore." %
+                server)
+            mailer.sendMail(recipient, sender,
+                "Minecraft Server Restart Failed - Muting until restore",
+                "%s restart failed.\r\nMuting until restore." % server)
+            mute = True
+            followup = False
+          if reboot:
+            result = subprocess.Popen("ssh mc@%s \"./forcestop_start.sh\"" %
+                server, shell=True)
+            logger.logMessage(log, "Server restart attempted.")
+            logger.logMessage(log, "%s" % result)
+            if not mute:
+              mailer.sendMail(recipient, sender,
+                  "Minecraft Server Down - Restart Attempted",
+                  "%s was not responding and a restart has been attempted.\r\nCheck log for details."
+                  % server)
+            reboot = False
+            followup = True
+          else:
+            reboot = True
+            logger.logMessage(log, "Server may be down. No reboot.")
+        else:
+          logger.logMessage(log, "Server Check OK")
+          reboot = False
+          if followup:
+            mailer.sendMail(recipient, sender,
+                "Server Restart Successful",
+                "%s restart successful.\r\nServer is back online." % server)
+            followup = False
+          mute = False
+      time.sleep(wait_time)
