63dc80a212ffe0a88b35bbd200d2376c858ee8a6
mbernhardt6 <mbernhardt6@gmail.com>
2015-10-29 18:25:24 -0700

    Theoretical Changes for GCE
    Some of these will likely need to be fixed.

--- mailer.py
+++ mailer.py
@@ -1,9 +1,25 @@
 import smtplib
+from gmail_auth import *
+from simplecrypt import decrypt
 
 def sendMail(recipient, sender, subject, message):
-  host = "mail.transcendedlife.local"
-  
-  body = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (sender, recipient, subject, message)
-  
+  """Wrapper to send mail.
+
+  Args:
+    recipient: Recipient of the message.
+    sender: Sender of the message.
+    subject: Subject of the message.
+    message: Message body.
+  """
+  host = "smtp.gmail.com:587"
+
+  body = "From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (sender, recipient,
+      subject, message)
+
+  password = decrypt('password', ciphertext)
+
   smtpObj = smtplib.SMTP(host)
+  smtpObj.starttls()
+  smtpObj.login(username,password)
   smtpObj.sendmail(sender, recipient, body)
+  smtpObj.quit()
--- mc_monitor.py
+++ mc_monitor.py
@@ -12,18 +12,18 @@ import subprocess
 import time
 
 # Variables
-server = "mortimer"
-port = "25565"
-recipient = "mark@transcendedlife.local"
-sender = "feros@transcendedlife.local"
-log = "/var/log/mc_monitor.log"
-base_path = "/home/mark/mc_monitor"
-nocheck = base_path + "nocheck"
-pid_file = "/tmp/mc_monitor.pid"
+server = 'localhost'
+port = '25565'
+recipient = 'mbernhardt6@gmail.com'
+sender = 'mbernhardt6@gmail.com'
+log = '/home/minecraft/scripts/mc_monitor.log'
+base_path = '/home/minecraft/scripts/mc_monitor'
+nocheck = base_path + 'nocheck'
+pid_file = '/tmp/mc_monitor.pid'
 wait_time = 300
 mute_reset = 12
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   # Is a reboot required?
   reboot = False
   # Will the script follow up after reboot?
@@ -33,46 +33,50 @@ if __name__ == "__main__":
   # Loop count to determine when to reset mute.
   mute_loop = 0
   if filer.SetPid(pid_file, log):
-    logger.logMessage(log, "Pid file set. Starting monitor.")
+    logger.logMessage(log, 'Pid file set. Starting monitor.')
     while 1:
       if not (os.path.isfile(nocheck)):
         status = nettools.checkPort(server, port)
-        if (status != "open"):
+        if (status != 'open'):
           if followup and not mute:
-            logger.logMessage(log, "%s restart attempted. Muting until restore." %
-                server)
+            logger.logMessage(log,
+                              '%s restart attempted. Muting until restore.' %
+                              server)
             mailer.sendMail(recipient, sender,
-                "Minecraft Server Restart Failed",
-                "%s restart failed.\r\nWill remind again after %s attempts." % (server, mute_reset))
+                            'Minecraft Server Restart Failed',
+                            '%s restart failed.\r\n'
+                            'Will remind again after %s attempts.' %
+                            (server, mute_reset))
             mute = True
             followup = False
           if reboot:
-            result = subprocess.Popen("ssh mc@%s \"./forcestop_start.sh\"" %
-                server, shell=True)
-            logger.logMessage(log, "Server restart attempted.")
-            logger.logMessage(log, "%s" % result)
+            result = subprocess.call('/home/minecraft/start-server.sh',
+                                     cwd='/home/minecraft')
+            logger.logMessage(log, 'Server restart attempted.')
+            logger.logMessage(log, '%s' % result)
             if not mute:
               mailer.sendMail(recipient, sender,
-                  "Minecraft Server Down - Restart Attempted",
-                  "%s was not responding and a restart has been attempted.\r\nCheck log for details."
-                  % server)
+                              'Minecraft Server Down - Restart Attempted',
+                              '%s was not responding and a restart has been '
+                              'attempted.\r\nCheck log for details.' % server)
             reboot = False
             followup = True
           else:
             reboot = True
-            logger.logMessage(log, "Server may be down. No reboot.")
+            logger.logMessage(log, 'Server may be down. No reboot.')
         else:
-          logger.logMessage(log, "Server Check OK")
+          logger.logMessage(log, 'Server Check OK')
           reboot = False
           if followup:
             mailer.sendMail(recipient, sender,
-                "Server Restart Successful",
-                "%s restart successful.\r\nServer is back online." % server)
+                            'Server Restart Successful',
+                            '%s restart successful.\r\nServer is back online.' %
+                            server)
             followup = False
           mute = False
       if mute:
         if mute_loop > mute_reset:
-          logger.logMessage(log, "Unsetting mute.")
+          logger.logMessage(log, 'Unsetting mute.')
           mute = False
           mute_loop = 0
         else:
